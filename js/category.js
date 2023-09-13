b1affc6873ddba53a5c5598b3708504ba997c946
mehrdad <ps4mehrdad@gmail.com>
2023-09-14 01:07:14 +0330

    fixed style addresses

--- js/category.js
+++ js/category.js
@@ -1,7 +1,7 @@
 const template = document.createElement("template");
 
 template.innerHTML = `
-        <link rel="stylesheet" href="../css/category-style.css">
+        <link rel="stylesheet" href="./css/category-style.css">
         <div class="category-item">
             <h1 class"category"><slot></slot></h1>
         </div>
